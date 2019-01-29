84f97b60a80dc7dd05cb65def56b5710ccbe9be5
Saman Ashkiani <sa.ashkiani@gmail.com>
2019-01-29 15:25:21 -0800

    Add initial test code

---
+++ test/test_slab_hash.cu
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 Saman Ashkiani
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <cuda_runtime_api.h>
+#include <cuda.h>
+
+#define DEVICE_ID 0
+
+int main(int argc, char** argv){
+	//=========
+	int devCount;
+  cudaGetDeviceCount(&devCount);
+  cudaDeviceProp devProp;
+  if(devCount){
+    cudaSetDevice(DEVICE_ID); // be changed later
+    cudaGetDeviceProperties(&devProp, DEVICE_ID);
+  }
+  printf("Device: %s\n", devProp.name);
+  return 0;
+}
