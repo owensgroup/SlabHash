e8acb9993508c216ec1a4c2ad0d6786dc466ee00
Saman Ashkiani <sa.ashkiani@gmail.com>
2019-07-21 20:00:10 -0700

    Use the CommandLine.h file to handle cmd line arguments

--- bench/main_benchmarks.cu
+++ bench/main_benchmarks.cu
@@ -24,21 +24,8 @@
 #include <sstream>
 #include <string>
 
+#include "CommandLine.h"
 #include "experiments.cuh"
-//
-inline char* getCmdOption(char** begin, char** end, const std::string& option) {
-  char** itr = std::find(begin, end, option);
-  if (itr != end && ++itr != end) {
-    return *itr;
-  }
-  return 0;
-}
-//=======================
-inline bool cmdOptionExists(char** begin,
-                            char** end,
-                            const std::string& option) {
-  return std::find(begin, end, option) != end;
-}
 
 int main(int argc, char** argv) {
   int mode = 0;  // type of experiment
